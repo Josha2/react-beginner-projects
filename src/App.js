4942add16dacc74471483953488081b719070ece
Josha <antonfask@gmail.com>
2024-05-25 21:49:08 +0700

    count2

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import './index.scss';
 function App() {
   const [count, setCount] = useState(0);
 
+  const countInc = () => {
+    setCount(count + 1);
+  };
+
+  const countDec = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
         <h1>{count}</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <button className="minus" onClick={countDec}>
+          - Минус
+        </button>
+        <button className="plus" onClick={countInc}>
+          Плюс +
+        </button>
       </div>
     </div>
   );
